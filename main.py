6a16e5f66cd9357d5c6a69cdf327b8af08bd6bf9
JROdeyemi <109704981+JROdeyemi@users.noreply.github.com>
2022-10-19 00:12:39 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print('Hey there, let us find you a rap name')
+#2. Ask the user for the city he/she lives in.
+City = input('What city do you live in? \n')
+#3. Ask the user for the last food they ate.
+Meal = input('What meal did you eat last? \n')
+#4. Message
+print('Your rap name is...')
+#5. Combine the name of their city and pet and show them their band name.
+print (City + " " + Meal)
+
